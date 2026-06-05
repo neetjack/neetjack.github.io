553bb395e1f97ce3463ef42cc2f9221ef6c60fc8
neetjack <neetjack1022@gmail.com>
2026-06-06 00:24:01 +0900

    debug 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://neetjack.github.io/",
-	base: "/",
+	base: "/selfblog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
